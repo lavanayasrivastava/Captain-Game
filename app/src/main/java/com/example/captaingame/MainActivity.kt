c3181fb6b0fb6c5813cb40977ff573eafcf81d18
Lavanya <lavanya.sh1303@gmail.com>
2024-12-13 12:53:53 +0530

    Final

--- app/src/main/java/com/example/captaingame/MainActivity.kt
+++ app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +18,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.captaingame.ui.theme.CaptainGameTheme
 import kotlin.random.Random
 
@@ -43,30 +52,33 @@ fun Captaingame(modifier: Modifier){
     val direction= remember{ mutableStateOf("north") }
     val stromORtressure= remember { mutableStateOf("") }
     val  health= remember { mutableStateOf(100) }
+    var decor=TextStyle(fontFamily = FontFamily.Default,
+        fontSize = 30.sp,
+        color = Color.Blue
+
+    )
+
 
     Column (modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Text("tressure found: ${tressurefound.value}")
-        Text("strom encountered ${stromencountered.value}")
-        Text("current direction: ${direction.value}")
+    {if(health.value>0) {
+        Text("CAPTAIN GAME", modifier = Modifier.padding(80.dp), style = decor,fontWeight = FontWeight.Bold)
+        Text("")
+        Text("Tressure found: ${tressurefound.value}")
+        Text("Strom encountered ${stromencountered.value}")
+        Text("Current direction: ${direction.value}")
         Text("HP ${health.value}")
         Text(stromORtressure.value)
-
-        if(health.value==0){
-            Text("GAME OVER")
-        }
 
         Button(onClick = {
             direction.value = "east"
             if (Random.nextBoolean()) {
                 tressurefound.value += 1
-                health.value+=5
-            }
-            else{
-                stromencountered.value+=1
-                health.value-=10
+                health.value += 5
+            } else {
+                stromencountered.value += 1
+                health.value -= 10
             }
 
         })
@@ -77,13 +89,12 @@ fun Captaingame(modifier: Modifier){
 
         Button(onClick = {
             direction.value = "west"
-            if(Random.nextBoolean()){
-                tressurefound.value+=1
-                health.value+=5
-            }
-            else{
-                stromencountered.value+=1
-                health.value-=10
+            if (Random.nextBoolean()) {
+                tressurefound.value += 1
+//                health.value+=5
+            } else {
+                stromencountered.value += 1
+                health.value -= 10
 
             }
         })
@@ -91,18 +102,15 @@ fun Captaingame(modifier: Modifier){
             Text("WEST")
 
 
-
-
-}
+        }
         Button(onClick = {
             direction.value = "north"
-            if(Random.nextBoolean()){
-                tressurefound.value+=1
-                health.value+=5
-            }
-            else{
-                stromencountered.value+=1
-                health.value-=10
+            if (Random.nextBoolean()) {
+                tressurefound.value += 1
+                health.value += 5
+            } else {
+                stromencountered.value += 1
+                health.value -= 10
             }
         })
         {
@@ -111,17 +119,30 @@ fun Captaingame(modifier: Modifier){
 
         Button(onClick = {
             direction.value = "south"
-            if(Random.nextBoolean()){
-                tressurefound.value+=1
-                health.value+=5
-            }
-            else{
-                stromencountered.value+=1
-                health.value-=10
+            if (Random.nextBoolean()) {
+                tressurefound.value += 1
+                health.value += 5
+            } else {
+                stromencountered.value += 1
+                health.value -= 10
             }
         })
         {
             Text("SOUTH")
+        }
+    }
+        else{
+            health.value=0
+            Text("👍 You Have Found ${tressurefound.value} tressures", fontWeight = FontWeight.Bold)
+            Text("😑 You Have Encountered ${stromencountered.value} Stroms",fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text("GAME OVER ")
+            Button(onClick = {tressurefound.value=0
+            stromencountered.value=0
+                health.value=100
+            })
+            { Text("RETRY") }
+
         }
 }
 }
